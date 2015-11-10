2d8624c8d56b861e28986815d06d952e99f521c5
ethan <ethanmannette@gmail.com>
2015-11-10 13:24:15 -0700

    init

---
+++ client/js/game/game.static.js
@@ -0,0 +1,18 @@
+/*
+ * Game Constants
+ *
+ * This file contains static constants that don't change
+ */
+
+window.game = window.game || {};
+
+window.game.static = {
+	colors: {
+		black: 0x000000,
+		white: 0xffffff,
+		green: 0x0fdb8c,
+		cyan: 0x38FDD9,
+		red: 0xff3300,
+		yellow: 0xe4e227
+	}
+};
