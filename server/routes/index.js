c12edf40b3108ac6423ec8b31fc35948a70e1469
ethan <ethanmannette@gmail.com>
2015-11-13 11:27:56 -0700

    set up very basic routes for game

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+  res.render('layout');
+});
+
+router.get('/game', function(req, res, next) {
   res.render('myDemo');
 });
 
