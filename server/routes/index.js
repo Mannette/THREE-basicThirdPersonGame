f128304806d54304b502f8c28501732a7e773f4c
ethan <ethanmannette@gmail.com>
2015-12-04 12:19:09 -0700

    added a 'catch all' to redirect user to the homepage if none of set routes are hit. Added an authentication and login for registration so user doesn't have to log in after they have registered. Temp users are registered with pre determined username and password.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,12 @@ var express = require('express'),
     router = express.Router(),
     passport = require('passport'),
     User = require('../models/user'),
+    path = require('path'),
     nonUser = require('../models/nonUser');
 
 
-router.get('/', function(req, res, next) {
-  res.render('layout');
+router.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../../client/index.html'));
 });
 
 router.post('/login', function(req, res, next) {
@@ -26,28 +27,52 @@ router.post('/login', function(req, res, next) {
 
 router.post('/register', function(req, res, next) {
   User.register(new User({
-    username: req.body.username
+    username: req.body.username, highscore: 0
   }), req.body.password, function(err, account) {
     if (err) {
+      console.log(err);
       return res.status(500).json({err: err});
     }
-    passport.authenticate('local')(req, res, function() {
-      return res.status(200).json({status: 'Registration successful!'});
-    });
+    passport.authenticate('local', function(error, user, info) {
+      if (error) { return next(error); }
+      if (!user) {
+        return res.status(404).json({error: info});
+      }
+      req.logIn(user, function(errors) {
+        if (errors) {
+          return res.status(500).json({errors: 'Could not log in user'});
+        }
+        return res.status(200).json({status: 'Registration successful!'});
+      });
+      // return res.status(200).json({status: 'Registration successful!'});
+    })(req, res, next);
   });
 });
 
 router.post('/tempUser', function(req, res, next) {
-  nonUser.register(new tempUser({}), function(err, account) {
+  User.findOne({username:'temp'}, function(err, user) {
     if (err) {
       return res.status(500).json({err: err});
     }
-    return res.status(200).json({status: 'Time to play!'});
+    if (user) {
+      return res.status(200).json({status: 'Temp registration successful!'});
+    } else {
+      User.register(new User({
+        username: 'temp', highscore: 0
+      }), 'temp', function(error, account) {
+        if (error) {
+          return res.status(500).json({err: err});
+        }
+        passport.authenticate('local', function(errors, response, info) {
+          return res.status(200).json({status: 'Temp registration successful!'});
+        });
+      });
+    }
   });
 });
 
-router.get('/#/game', function(req, res, next) {
-  res.render('myDemo');
-});
+// router.get('/game', function(req, res, next) {
+//   res.render('myDemo');
+// });
 
 module.exports = router;
